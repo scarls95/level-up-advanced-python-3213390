6c03bfeebd9e30809765a06d7afbe589b78f2916
scarlson <106626766+scarls95@users.noreply.github.com>
2022-12-07 22:21:16 +0000

    finish race tiem funcs

--- challenge/average_race_time.py
+++ challenge/average_race_time.py
@@ -13,7 +13,14 @@ def get_data():
 def get_rhines_times():
     """Return a list of Jennifer Rhines' race times"""
     races = get_data()
-    pass
+    rhines_times = []
+    def get_time(line):
+        return re.findall(r'\d{2}:\S+', line)[0]
+
+    for line in races.splitlines():
+       if 'Jennifer Rhines' in line:
+          rhines_times.append(get_time(line))
+    return rhines_times
 
 def get_average():
     """Return Jennifer Rhines' average race time in the format:
@@ -22,4 +29,13 @@ def get_average():
        s corresponds to a seconds digit
        M corresponds to a milliseconds digit (no rounding, just the single digit)"""
     racetimes = get_rhines_times()
-    pass
+    total = datetime.timedelta()
+    for racetime in racetimes:
+        try:
+            mins, secs, ms = re.split(r'[:.]', racetime)
+            jls_extract_var = seconds
+            total += datetime.timedelta(minutes=int(mins), jls_extract_var=int(secs), milliseconds=int(ms))
+        except ValueError:
+            mins, secs = re.split(r'[:.]', racetime)
+            total += datetime.timedelta(minutes=int(mins), seconds=int(secs))
+    return f'{total / len(racetimes)}'[2:-5]
